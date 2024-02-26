ace05bc66441092a90f0590836def47e21be768b
Javier Salazar <javierandre117@gmail.com>
2024-02-25 21:36:25 -0600

    Mejoras en impresión del triangulo
    Se mejoró la forma de mostrar el triangulo de Fibonacci. Además de que se cambió la estructura básica del proyecto.

--- proyecto1/src/main/java/com/example/App.java
+++ proyecto1/src/main/java/com/example/App.java
@@ -5,8 +5,8 @@ import com.example.Maquina.Maquina;
 public class App 
 {
     public static void main(String[] args) {
-        Maquina maquina = new Maquina();
-        maquina.construirArbol(30); // Asumiendo que quieres construir un árbol para n = 5
-        Maquina.printTree(maquina.raiz); // Imprime el árbol empezando por la raíz
+        Maquina maquina = new Maquina(49);
+        maquina.result();
+        maquina.txtSaver();
     }
 }
